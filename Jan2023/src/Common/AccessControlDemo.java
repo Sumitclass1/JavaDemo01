74252a86102309e17a858aceebc9b7dd6f2d9277
Sumit <meshramsumit162@gmail.com>
2023-02-11 00:25:42 +0530

    Program of Access Control demo

---
+++ Jan2023/src/Common/AccessControlDemo.java
@@ -0,0 +1,17 @@
+package Common;
+
+public class AccessControlDemo {
+	
+		int a; //Default acess
+		public int b; // public acess
+		private int c; // private acess
+		void setc (int i)
+		{
+			c=i;
+		}
+		int getc ()
+		{
+			return c;
+		}
+
+}
